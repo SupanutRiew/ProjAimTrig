8a7957d5cb066a88e62fe4ce7336b17f5225a1d9
Supanut S <64011636@kmitl.ac.th>
2021-10-26 22:55:48 +0700

    combine aim code and trig code

--- Aimandshoot.py
+++ Aimandshoot.py
@@ -1,3 +1,11 @@
+#bind CTRL "+duck;r_cleardecals"
+#bind shift "+speed;r_cleardecals"
+#bind mouse1 "+attack;r_cleardecals"
+#bind w "+forward;r_cleardecals"
+#bind s "+back;r_cleardecals"
+#bind d "+moveright;r_cleardecals"
+#bind a "+moveleft;r_cleardecals"
+
 import win32gui
 import numpy as np
 import pyautogui
@@ -30,25 +38,101 @@ elif oldsetting == "3":
     reswidth = int(input("Width resolution: "))
     resheight = int(input("Height resolution: "))
 
-#all guns
-#gun type code from another file
-#choose guns
-chooseoption = input("AimBot, TriggerBot")
-if chooseoption == "TriggerBot":
-    #code from another file
-    print("To be Done")
-elif chooseoption == "AimBot":
-    time.sleep(5)
-    size_scale = 1
 
+chooseoption = input("AimBot, TriggerBot")
+
+
+if chooseoption == "TriggerBot":
+    #all guns
+    def bulldog():
+        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        time.sleep(0.25)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0)
+    def marshal():
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        time.sleep(0.01)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+    def spraytime(x):
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        time.sleep(int(x))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+    def operator():
+        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0)
+        time.sleep(0.25)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0)
+    def sprayaim(x):
+        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        time.sleep(int(x))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0)
+    #choose guns
+    gun = input("1. aim&tap 2. tap 3. aim&burst 4. spray 5. aim&spray")
+    if gun == "1":
+        def howshoot():
+            operator()
+    elif gun == "2":
+        def howshoot():
+            marshal()
+    elif gun == "3":
+        def howshoot():
+            bulldog()
+    elif gun == "4":
+        sec = input("How many seconds to spray once detected?")
+        def howshoot():
+            spraytime(sec)
+    elif gun == "5":
+        sec = input("How many seconds to spray once detected?")
+        def howshoot():
+            sprayaim(sec)
     while True:
         #Take Screenshott
-        jimmy = np.array(pyautogui.screenshot(region=(0,0,reswidth,resheight)))
+        jimmy = np.array(pyautogui.screenshot(region=(0,0,reswidth,newresheight)))
         jimmy = cv2.cvtColor(jimmy, cv2.COLOR_BGR2RGB)
         #Resize only for better understanding
         jimmy = cv2.resize(jimmy, (jimmy.shape[1]// size_scale, jimmy.shape[0]//size_scale))
-        image = np.expand_dims(jimmy, 0)
-        img_w, img_h = image.shape[2], image.shape[1]
+
+
+        #detect object
+        ClassIndex, confidece, bbox = model.detect(jimmy)
+        
+        for i, box in enumerate(bbox):
+                #see class from labels file
+                if ClassIndex[i] == 1 and confidece[i] >= 0.4:
+                    ymin, xmin, ymax, xmax = tuple(box)
+                    for ClassInd, conf, boxes in zip(ClassIndex, confidece, bbox):
+                                    #current issue: box position done
+                        left, right, top, bottom = int(xmin), int(xmin + xmax), int(ymin), int(ymin + ymax)
+                        cv2.rectangle(jimmy, (int(top), int(left)), (int(bottom), int(right)), (125, 255, 51), thickness=2)
+            
+                        print("Detected", len(ClassIndex))
+                        print(left, right, top, bottom)
+                                      #192 260 326 353  middle is 341.5 192
+                    if left <= resheight/(2*size_scale) and right >= resheight/(2*size_scale) and top <= reswidth/(2*size_scale) and bottom >= reswidth/(2*size_scale):
+                        howshoot()                              #top, left, left2right, topdown
+                                                                                                                # /2 scale 446 274 43 105  tlbr real scale   892, 548, 86, 210
+                                                                                                                #top, left, left + xmax = right, top + ymax = down
+        cv2.imshow("jimmy", jimmy)                       
+        cv2.waitKey(1)
+
+
+
+    
+elif chooseoption == "AimBot":
+    time.sleep(5)
+    size_scale = 1
+    newresheight = 0.8 * resheight
+    while True:
+        #Take Screenshott
+        jimmy = np.array(pyautogui.screenshot(region=(0,0,reswidth,newresheight)))
+        jimmy = cv2.cvtColor(jimmy, cv2.COLOR_BGR2RGB)
+        #Resize only for better understanding
+        jimmy = cv2.resize(jimmy, (jimmy.shape[1]// size_scale, jimmy.shape[0]//size_scale))
+
 
         #detect object
         ClassIndex, confidece, bbox = model.detect(jimmy)
@@ -57,7 +141,7 @@ elif chooseoption == "AimBot":
         #get box position
         for i, box in enumerate(bbox):
             #see class from labels file
-            if ClassIndex[i] == 1 and confidece[i] >= 0.4:
+            if ClassIndex[i] == 1 and confidece[i] >= 0.35:
                 ymin, xmin, ymax, xmax = tuple(box)
                 for ClassInd, conf, boxes in zip(ClassIndex, confidece, bbox):
                                 #current issue: box position done
@@ -71,19 +155,23 @@ elif chooseoption == "AimBot":
                     print(left, right, top, bottom)
                                     #192 260 326 353  middle is 341.5 192
                     midofleftright = (left+right)/2
-                    midoftopbottom = (top+bottom)/2
+                    eightyshoulder = 0.1 * (bottom-top)
+                    midoftopbottom = top + eightyshoulder
                     print(midofleftright, midoftopbottom)
                     print(reswidth/2, resheight/2)
                     # now we have pixel position of detected object
-                    
+
                     #next is to find the pixel differences between the crosshair and move mouse
 
                     pixeldiffx = int(round(midofleftright-(reswidth/2),0))
                     pixeldiffy = int(round(midoftopbottom-(resheight/2),0))
                     print(pixeldiffx, pixeldiffy)
-                    #win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, pixeldiffx, pixeldiffy, 0, 0)
-                    #win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-                    #time.sleep(0.01)
-                    #win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+                    #mouse movement does not move according to the pixel given accurately.
+                #ได้แล้ววววววววววววำสา่พืเรฟหีำนพืเาฟ่หกิพเสฟำิอสรฟำพเสฟ
+                win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, pixeldiffx, pixeldiffy, 0, 0)
+                time.sleep(0.01)
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+                time.sleep(0.01)
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
         cv2.imshow("jimmy", jimmy)                       
         cv2.waitKey(1)
