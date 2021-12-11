a509be1d31bdbbcdd0b268a6ed26651f70f80966
Supanut S <64011636@kmitl.ac.th>
2021-12-11 14:42:43 +0700

    remove unused imports

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QFileDialog, QLabel
+from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog
 from PyQt5.QtGui import QPixmap
 import sqlite3
 #DONE  
